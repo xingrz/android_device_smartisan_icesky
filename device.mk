f24d69e6bad4143424735e2896238ccfa0e09cc2
Luca Stefani <luca020400@lineageos.org>
2017-08-27 20:25:32 +0200

    icesky: Set TARGET_NEEDS_LEGACY_CAMERA_HAL1_DYN_NATIVE_HANDLE
    * This is required to enable our legacy HAL1 camera recording
      following OMX hardening in Ief2d2d4d0445074c0ecdc723b1b25997a7631389
    
    Change-Id: Ide8dad10bf72fbd1acefec2f8ef4a90b0e3c0a4b

--- device.mk
+++ device.mk
@@ -297,6 +297,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl

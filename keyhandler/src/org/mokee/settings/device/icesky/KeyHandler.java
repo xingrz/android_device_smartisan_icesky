d127208093e899e74345d04af3b41a8b9227a7ad
XiNGRZ <chenxingyu92@gmail.com>
2019-06-06 21:47:05 +0800

    icesky: keyhandler: Avoid accident triggering while holding both side keys
    Change-Id: I8ecebd4c33212c4a28af934327719227f3f68223

--- keyhandler/src/org/mokee/settings/device/icesky/KeyHandler.java
+++ keyhandler/src/org/mokee/settings/device/icesky/KeyHandler.java
@@ -47,6 +47,7 @@ public class KeyHandler implements DeviceKeyHandler {
     private static final int KEY_RIGHT_UP = 3;
     private static final int KEY_RIGHT_DOWN = 4;
 
+    private final int singleTapTimeout = 150;
     private final int longPressTimeout = ViewConfiguration.getLongPressTimeout();
     private final int longLongPressTimeout = 2000;
     private final int keyRepeatDelay = ViewConfiguration.getKeyRepeatDelay();
@@ -66,6 +67,8 @@ public class KeyHandler implements DeviceKeyHandler {
     private ScreenshotHelper screenshotHelper;
 
     private boolean ongoingPowerLongPress = false;
+    private boolean pendingLeftSingleTap = false;
+    private boolean pendingRightSingleTap = false;
 
     private int leftIndexPressed = -1;
     private int rightIndexPressed = -1;
@@ -165,14 +168,23 @@ public class KeyHandler implements DeviceKeyHandler {
             leftIndexPressed = -1;
         }
 
+        pendingRightSingleTap = false;
+        handler.removeCallbacksAndMessages("right_tap");
+        handler.removeCallbacksAndMessages("right_repeat");
+
         switch (event.getAction()) {
             case KeyEvent.ACTION_DOWN:
                 if (rightIndexPressed != -1) {
-                    injectKey(keys[rightIndexPressed].keyCode, KeyEvent.ACTION_UP, KeyEvent.FLAG_CANCELED);
-                    handler.removeCallbacksAndMessages("right_repeat");
                     handler.postDelayed(triggerPartialScreenshot, "partial_screenshot", longPressTimeout);
                 } else {
-                    injectKey(matchedKey.keyCode, KeyEvent.ACTION_DOWN, 0);
+                    pendingLeftSingleTap = true;
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            pendingLeftSingleTap = false;
+                            injectKey(matchedKey.keyCode, KeyEvent.ACTION_DOWN, 0);
+                        }
+                    }, "left_tap", singleTapTimeout);
                     scheduleKeyRepeat(matchedKey.keyCode, "left_repeat");
                 }
                 if (pm.isInteractive()) {
@@ -184,6 +196,11 @@ public class KeyHandler implements DeviceKeyHandler {
                     takeScreenshot(false);
                     rightIndexPressed = -1;
                 } else {
+                    if (pendingLeftSingleTap) {
+                        pendingLeftSingleTap = false;
+                        handler.removeCallbacksAndMessages("left_tap");
+                        injectKey(matchedKey.keyCode, KeyEvent.ACTION_DOWN, 0);
+                    }
                     injectKey(matchedKey.keyCode, KeyEvent.ACTION_UP, 0);
                 }
                 handler.removeCallbacksAndMessages("left_repeat");
@@ -215,14 +232,23 @@ public class KeyHandler implements DeviceKeyHandler {
             rightIndexPressed = -1;
         }
 
+        pendingLeftSingleTap = false;
+        handler.removeCallbacksAndMessages("left_tap");
+        handler.removeCallbacksAndMessages("left_repeat");
+
         switch (event.getAction()) {
             case KeyEvent.ACTION_DOWN:
                 if (leftIndexPressed != -1) {
-                    injectKey(keys[leftIndexPressed].keyCode, KeyEvent.ACTION_UP, KeyEvent.FLAG_CANCELED);
-                    handler.removeCallbacksAndMessages("left_repeat");
                     handler.postDelayed(triggerPartialScreenshot, "partial_screenshot", longPressTimeout);
                 } else {
-                    injectKey(matchedKey.keyCode, KeyEvent.ACTION_DOWN, 0);
+                    pendingRightSingleTap = true;
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            pendingRightSingleTap = false;
+                            injectKey(matchedKey.keyCode, KeyEvent.ACTION_DOWN, 0);
+                        }
+                    }, "right_tap", singleTapTimeout);
                     scheduleKeyRepeat(matchedKey.keyCode, "right_repeat");
                 }
                 if (pm.isInteractive()) {
@@ -234,6 +260,11 @@ public class KeyHandler implements DeviceKeyHandler {
                     takeScreenshot(false);
                     leftIndexPressed = -1;
                 } else {
+                    if (pendingRightSingleTap) {
+                        pendingRightSingleTap = false;
+                        handler.removeCallbacksAndMessages("right_tap");
+                        injectKey(matchedKey.keyCode, KeyEvent.ACTION_DOWN, 0);
+                    }
                     injectKey(matchedKey.keyCode, KeyEvent.ACTION_UP, 0);
                 }
                 handler.removeCallbacksAndMessages("right_repeat");

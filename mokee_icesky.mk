eed15c96ef8ea4553297811aaf3a81c018c2e5d8
XiNGRZ <chenxingyu92@gmail.com>
2019-09-16 13:09:04 +0800

    icesky: Rename mk -> mokee
    Change-Id: Icc6126c15d068270105330c21acaf2f62eed0fb7

--- mokee_icesky.mk
+++ mokee_icesky.mk
@@ -22,13 +22,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l_mr1.mk
 # Inherit from icesky device
 $(call inherit-product, device/smartisan/icesky/device.mk)
 
-# Inherit some common MK stuff.
-$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+# Inherit some common MoKee stuff.
+$(call inherit-product, vendor/mokee/config/common_full_phone.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.mk.maintainer=XiNGRZ
 
-PRODUCT_NAME := mk_icesky
+PRODUCT_NAME := mokee_icesky
 PRODUCT_BRAND := SMARTISAN
 PRODUCT_DEVICE := icesky
 PRODUCT_MANUFACTURER := smartisan
